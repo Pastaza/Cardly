87c61fef36605d6f2045c1e74ccc02716fc044c4
Pasta <hogg.zachary@icloud.com>
2025-11-09 12:57:53 +1300

    Delete src directory

--- src/main/java/com/cardly/manual/Card.kt
+++
@@ -1,9 +0,0 @@
-package com.cardly.manual
-
-import java.io.Serializable
-
-data class Card(
-    var brand: String = "",
-    var expiryMillis: Long = 0L,
-    var notes: String = ""
-) : Serializable
